c322d3029d06a144ecdbe89fa62ba301db24045a
Aleksei Korepanov <aleksei_korepanov@epam.com>
2019-12-24 11:07:51 +0400

    first commit

---
+++ src/test/Autotest.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Autotest {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("http://blog.csssr.ru/qa-engineer/");
+
+        JavascriptExecutor scroll = driver;
+        scroll.executeScript("window.scrollBy(0,600)"); //скроллим ниже
+
+        WebElement element = driver.findElement(By.xpath(".//div[@class='graphs-errors']/a")); //находим второй блок
+        element.click();
+
+        //проверяем гиперссылку
+        try {
+            element = driver.findElement(By.xpath(".//a[@href='http://monosnap.com/']"));
+        } catch (NoSuchElementException e) {
+            System.out.println("Wrong reference!");
+        }
+    }
+}
